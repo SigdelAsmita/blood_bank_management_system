1caccdb627748aed032a8df312b91a53bcdff724
Aviyanshu <aviyanshuadhikari@gmail.com>
2023-07-10 23:36:10 +0545

    Changed few stuff

--- Blood_Bank/settings.py
+++ Blood_Bank/settings.py
@@ -12,16 +12,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_uds45-7+(6suvljo7q0cruimy4^((p+g#_!4=8&8f6h&##_g3'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,17 +36,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bank',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bank.apps.BankConfig',
-    'users.apps.UsersConfig',
+    # 'bank.apps.BankConfig',
+    # 'users.apps.UsersConfig',
     'crispy_forms',
-    'crispy_bootstrap4'
+    # 'crispy_bootstrap4'
 ]
+
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -80,8 +88,12 @@ WSGI_APPLICATION = 'Blood_Bank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
 
---
+++ ready.sql
@@ -0,0 +1,19 @@
+INSERT INTO BLOOD_BANK VALUES (0001, 'Bhaktapur NRCS Blood Bank', 'Bhaktapur', '01-6611661');
+INSERT INTO BLOOD_BANK VALUES (0002, 'Central NRCS Blood Bank', 'Soaltee-Mode', '01-4288485');
+INSERT INTO BLOOD_BANK VALUES (0003, 'LalitpuR NRCS Blood Bank', 'Pulchowk', '+97715427033');
+INSERT INTO BLOOD_BANK VALUES (0004, 'Frontline Hospital Blood Bank', 'Old Baneshwor, Kathmandu', '01-5970132');
+INSERT INTO BLOOD_BANK VALUES (0005, 'Teaching Hospital', 'Maharajgunj, Kathmandu', '986-2737316');
+INSERT INTO BLOOD_BANK VALUES (0006, 'Grande Hospital', 'Dhapasi', '01-5159266');
+INSERT INTO BLOOD_BANK VALUES (0007, 'Prasuti Griha', 'Thapathali, Kathmandu', '01-4260405');
+INSERT INTO BLOOD_BANK VALUES (0008, 'Nepal Mediciti Hospital', 'Nakkhu Ukalo, Lalitpur', '01-4217766');
+INSERT INTO BLOOD_BANK VALUES (0009, 'Bir Hospital', 'New Road Gate, Kathmandu', '01-4221119');
+INSERT INTO BLOOD_BANK VALUES (0010, 'Nepal Police Hospital', 'Maharajgunj, Kathmandu', '01-4412430');
+INSERT INTO BLOOD_BANK VALUES (0011, 'Civil Hospital', 'Minbhawan, Kathmandu', '01-4107000');
+INSERT INTO BLOOD_BANK VALUES (0012, 'Patan Hospital', 'Patan, Lalitpur', '01-5522295');
+INSERT INTO BLOOD_BANK VALUES (0013, 'Birendra Army Hospital', 'Kathmandu', '01-4371941');
+INSERT INTO BLOOD_BANK VALUES (0014, 'Nepal Medical College', 'Gokarneshwor, Kathmandu', '01-4911008');
+INSERT INTO BLOOD_BANK VALUES (0015, 'Kathmandu Medical College and Teaching Hospital', 'Sinamangal, Kathmandu', '01-4469064');
+
+        
+
+        
