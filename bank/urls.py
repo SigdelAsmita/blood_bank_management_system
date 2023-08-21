822d060e6ddf68e19fa8324516fb40a05b162f0c
ASMITA SIGDEL <asmitasigdel13@gmail.com>
2023-08-21 21:12:21 +0545

    Initial data added

--- bank/urls.py
+++ bank/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name ='bank-home'),
     path('about/', views.about, name ='bank-about'),
+    path('search', views.search, name ='bank-search'),
     path('bloodbanks/', views.bloodbank, name ='bank-bloodbank'),
 ]
--- bank/views.py
+++ bank/views.py
@@ -17,3 +17,11 @@ def bloodbank(request):
     cards=[]
     blood_bank_list=BloodBank.objects.all()
     return render(request, 'bank/bloodbank.html', context={"blood_bank":blood_bank_list, 'title':'BloodBanks'})
+
+def search(request):
+    if request.method =="POST":
+        searched = request.POST['searched']
+        banks = BloodBank.objects.filter(blood_bank_name__contains= searched)
+        return render (request, 'bank/search.html', {'title': 'Search','searched':searched, 'banks': banks})
+    else:
+        return render (request, 'bank/search.html', {'title': 'Search'})
