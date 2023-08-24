4ebe9aebc1b363a71b2b8c8d9f1af468b5bd738b
ASMITA SIGDEL <asmitasigdel13@gmail.com>
2023-08-24 16:50:43 +0545

    Home Page Stories update

--- Blood_Bank/settings.py
+++ Blood_Bank/settings.py
@@ -12,21 +12,21 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import environ
+# import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
-#SECRET_KEY = 'django-insecure-_uds45-7+(6suvljo7q0cruimy4^((p+g#_!4=8&8f6h&##_g3'
+# SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-_uds45-7+(6suvljo7q0cruimy4^((p+g#_!4=8&8f6h&##_g3'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -85,28 +85,28 @@ WSGI_APPLICATION = 'Blood_Bank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': "Blood_Management",
-#         'USER': "postgres",
-#         'PASSWORD': "postgres",
-#         'HOST': "localhost",
-#         'PORT': "5432",
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "Blood_Management",
+        'USER': "postgres",
+        'PASSWORD': "postgres",
+        'HOST': "localhost",
+        'PORT': "5432",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
